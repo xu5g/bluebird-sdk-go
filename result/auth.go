e20cc54f6096ff35cb3a8d8c635d90d267d4ebb8
lxjme <963176319@qq.com>
2021-04-08 10:02:53 +0800

    获取token的结构体中增加一个expired字段（过期时间）

--- result/auth.go
+++ result/auth.go
@@ -3,7 +3,9 @@ package result
 // 获取token
 type AuthResult struct {
 	Result
-	Data    struct {
-		Token string `json:"token"` //权限令牌，调用其它接口时需要提供token，有效期为 1 小时，过期后无法使用
+	Data struct {
+		Token   string `json:"token"`   // 权限令牌，调用其它接口时需要提供token，有效期为 1 小时，过期后无法使用
+		Expired string `json:"expired"` // 过期时间
+
 	} `json:"data,omitempty"` // 返回结果
 }
