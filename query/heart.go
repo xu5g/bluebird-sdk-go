b77b491f58f0b8a17672ad7a36d781dc8402563c
lxjme <963176319@qq.com>
2021-04-13 15:06:14 +0800

    体温、心率、血压时间参数改为非必填

--- query/heart.go
+++ query/heart.go
@@ -8,10 +8,10 @@ type HeartGetQuery struct {
 
 // 获取心率数据列表
 type HeartsGetQuery struct {
-	StartTime string // 开始时间	格式为2006-01-01 00:00:00（必传）
-	EndTime   string // 结束时间	格式为2006-01-01 00:00:00（必传）
 	Page      int32  // 页码	范围为1-5000（必传）
 	Limit     int32  // 每页条数	范围为1-100（必传）
+	StartTime string // 开始时间	格式为2006-01-01 00:00:00
+	EndTime   string // 结束时间	格式为2006-01-01 00:00:00
 	ImeiSn    string // 设备号	长度不超过20
 	Uuid      string // UUID	长度不超过40
 	Sort      string // 排序方向 DESC/ASC；默认倒序
--- query/track.go
+++ query/track.go
@@ -2,8 +2,8 @@ package query
 
 // 获取轨迹
 type TracksGetQuery struct {
-	StartTime string // 开始时间	格式为2006-01-01 00:00:00（必传）
-	EndTime   string // 结束时间	格式为2006-01-01 00:00:00（必传）
+	StartTime string // 开始时间	格式为2006-01-01 00:00:00
+	EndTime   string // 结束时间	格式为2006-01-01 00:00:00
 	Page      int32  // 页码		范围为1-5000（必传）
 	Limit     int32  // 每页条数	范围为1-100（必传）
 	Sort      string // 排序方向 DESC/ASC；默认倒序
