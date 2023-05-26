e6a8cd6a41c2a17228103692e6d3c4fc94bfe85d
刘鹏辉 <zhengdalph@163.com>
2023-05-26 09:57:07 +0800

    设备详情增加字段

--- result/device.go
+++ result/device.go
@@ -40,7 +40,7 @@ type DeviceEntity struct {
 	Remind            []interface{}  `json:"remind"`             // 闹钟数据
 	FirstlinkTime     string         `json:"firstlink_time"`     // 设备初次链接时间
 	HeartbeatUpload   int64          `json:"heartbeat_upload"`   // 心跳
-	Runstatus         int64          `json:"heartbeat_upload"`
+	Runstatus         int64          `json:"runstatus"`			 //运动静止状态
 }
 
 //设置睡眠时间段结构体
